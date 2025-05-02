6d5e3b57d487ae948915071e1675cf05be4d93c6
chetan <chetan.cloudwapp@gmail.com>
2025-05-02 19:40:07 +0530

    bucket operator

--- L18_Aggregate_lookup_operator.php
+++ L18_Aggregate_lookup_operator.php
@@ -1,9 +1,118 @@
 <?php
-
-
 /*
 
-LoopUp Operator :- It is working same as joins in mysql.now let suppose
+// ----------------- LOOKUP OPERATOR ----------------- //
+
+LoopUp Operator :- It is working same as joins in mysql, isme bhi hum two collection ko join krke data nikal skte hai. Now let suppose we have two tables students and library
+
+students collections contains :- id, name , class
+library collections contains :- id, book , student_id
+
+=> Now hum yhh janna chahte hai ki kis student ne konsi book li hai to uske liye hum mongodb mai lookup operator ka use krenge
+
+
+******************* SYNTAX OF LOOKUP OPERATOR ***********************
+
+db.library.aggregate([
+    {
+        $lookup: {
+            from: "student", // jiss dusri collections se library collection ko join krna chahte hai uska name aayga
+            localField: "student_id", // library ke andar aisi konsi local field hai jise hum student ki collection ke sath join karenge
+            foreignField: "_id", // student collection ki foreign id
+            as: "book_details" // output array field jo bhi hum data nikalenge use iss array mai daal denge iss arr ka name hum kuch bhi rakh skte hai
+        }
+    },
+    { $unwind : "$Student"} 
+])
+
+Note:- Unwind operator ka use hum array ko remove krne k liye krte hai iska use hum kahi bhi krskte hai.
+
+example :
+
+Before unwind => Student: [ { _id: 6, name: 'Sanjay Dutt', age: 21, class: 'BSC' } ]
+After unwind => Student: { _id: 6, name: 'Sanjay Dutt', age: 21, class: 'BSC' }
+
+
+// ----------------- REPLACEBOOT OPERATOR ----------------- //
+
+NOTE:- jab bhi hum lookup operator ke data ko dekhte hai output mai to use samjhna kaafi complex hojaata hai agar number of record bahut jyada hai to ise ko easy way mai dikhane k liye replaceboot operator ka use hota hai.
+
+Example :- Before Replace Boot operator
+
+{
+    _id:1,
+    book:"Atomic Habits",
+    student_id: 1,
+    Student: [
+        {
+            _id: 1,
+            name: "Sanjay Dutt",
+            age: 21,
+            class: "BSC"
+        }
+    ]
+}
+
+=> After Replace Boot Operator our output will be like this -;
+
+{
+    _id:1,
+    name: "Sanjay Dutt",
+    class: "BSC",
+    book:"Atomic Habits",
+    student_id: 1,
+}
+
+WORKING OF REPLACEBOOT OPERATOR :- Yhh 3 steps mai work krta hai sbse phle hume foreign table ka data nikalna padta hai and then after that hum local table ka data nikalte hai and then dono ko merge krte hai.
+
+our foreign data in this case :- 
+
+Student: [
+    {
+        _id: 1,
+        name: "Sanjay Dutt",
+        age: 21,
+        class: "BSC"
+    }
+]
+
+1). $replaceRoot: { // after that hum ise replace krte hai with the help of replaceRoot
+    newRoot: {  // and then hum uska new Root create krlete hai
+        $mergeObjects: [ // mergeObj dono data ko merge krdeta hai
+            { $arrayElemAt: ["$Student", 0] }, // yha par humne student ki collection ka data daal diya hai at zero index
+            "$$ROOT" // yha par humne library ki collection ka data daal diya
+        ]
+    }
+}
+
+******************** LOOKUP WITH REPLACEBOOT OPERATOR ***********************
+
+db.library.aggregate([
+    {
+        $lookup :{
+            from : "students",
+            localField:"student_id",
+            foreignField:"_id",
+            as:"Student" // library or student ka data join ho kr iss array mai aayega
+        }
+    },
+    {
+        $replaceRoot: {
+            newRoot: {
+                $mergeObjects: [
+                    { $arrayElemAt: ["$Student", 0] },
+                    "$$ROOT"
+                ]
+            }
+        }
+    },
+    {
+        $project:{ Student:0} // agar hum as:"Student" ko nahi dekhna chahte hai to hum ise 0 krke hata skte hai with the help of project operator
+    }
+])
+
 
 
 */
+
+
---
+++ L19_Aggregate_bucket_operator.php
@@ -0,0 +1,25 @@
+<?php
+/*
+
+// ----------------- BUCKET and Bucket AUTO OPERATOR ----------------- //
+
+=> Note :- working same as group operator but the difference is that isme hum jo grouping krte hai vo numeric field ki value pr hi krte hai
+
+let suppose we have this kind of data in our collections :-
+
+{ _id:1, name:"Sanjay Dutt", percentage: 90 }
+{ _id:2, name:"Shyam Dutt", percentage: 80 }
+{ _id:3, name:"Ram Dutt", percentage: 70 }
+{ _id:4, name:"Ajay Dutt", percentage: 60 }
+{ _id:5, name:"Amit Dutt", percentage: 50 }
+{ _id:6, name:"Raj Dutt", percentage: 40 }
+
+Now ab hum ye chahte hai ki jo bhi student ki percentage 90 se jyada hai unhe ek group mai daal do, jo 80 se jyada hai unhe dusre group mai daal do, jo 70 se jyada hai unhe dusre group mai daal do and so on i.e ek range ke hisab se count nikalte hai ise ko hum bucket kehte hai
+
+************** syntax of bucket operator ***************
+
+DB
+
+
+*/
+
